4c8015a2ffe25e01d97c43eef13d961b79c20817
mekhanonspotify-svg <mekhan.on.spotify@gmail.com>
2026-05-15 13:18:28 +0200

    Create install_dependencies.bat

---
+++ install_dependencies.bat
@@ -0,0 +1,16 @@
+@echo off
+title Spotify Remote Dependency Installer
+echo ==================================================
+echo   Installing Liquid Glass Project Dependencies
+echo ==================================================
+echo.
+echo Installing: pywebview, spotipy, colorthief, PyQt6, requests...
+echo.
+
+pip install pywebview spotipy colorthief PyQt6 requests
+
+echo.
+echo ==================================================
+echo   Process Finished! Check above for any errors.
+echo ==================================================
+pause
